7976044cb645ed2309de1da8bac3b81183b69dc5
Vika Putri Ariyanti <vikaputri909@gmail.com>
2023-08-15 09:49:41 +0700

    education-update

--- src/containers/Education/index.js
+++ src/containers/Education/index.js
@@ -1,6 +1,6 @@
 import React from "react";
 import Header from "../../components/Header";
-import UG from "../../assets/UG.jpg"
+import UG from "../../assets/UG.png"
 
 const Education = () => {
   return (
@@ -10,32 +10,36 @@ const Education = () => {
         <div class="d-flex justify-content-center">
           <i class="fa fa-graduation-cap fa-3x"></i><h1 class="ms-3">My Education</h1>
         </div>
-        <div className="py-3 container mt-4">
-          <div className="row justify-content-center">
-            <div class="card mb-4" style={{width : '40rem'}}>
-                <div className="row">
-                  <div className="col-md-4">
-                    <img className="card-img-top" src={UG} alt=""/>
+
+        <div className="py-3 container mt-4 d-flex justify-content-center">
+          <div className="">
+            <div class="card mb-4" style={{width : '45rem'}}>
+                <div className="row m-1">
+                  <div className="col-md-3">
+                  <img className="card-img-top" src={UG} alt=""/>
                   </div>
-                  <div className="col-md-8">
+                  <div className="col-md-9">
                     <h3>Gunadarma University</h3>
                     <div>Master Degree - Information System Management</div>
-                    <div className="text-muted">Sep 2021 - Jul 2023</div>
-                    <div>GPA : 3.93/4.0</div>
+                    <div className="text-muted mb-2">Sep 2021 - Jul 2023</div>
+                    <div><strong>GPA</strong> : 3.93/4.0</div>
+                    <div><strong>Thesis</strong> : Forecasting Crude Oil Prices using ARIMA and SARIMA Model</div>
                   </div>
                 </div>
               </div>
 
-              <div class="card" style={{width : '40rem'}}>
-                <div className="row">
-                  <div className="col-md-4">
+              <div class="card" style={{width : '45rem'}}>
+                <div className="row m-1">
+                  <div className="col-md-3">
                     <img className="card-img-top" src={UG} alt=""/>
                   </div>
-                  <div className="col-md-8">
+                  <div className="col-md-9">
                     <h3>Gunadarma University</h3>
                     <div>Bachelor Degree - Informatics Engineering</div>
-                    <div className="text-muted">Sep 2017 - Aug 2021</div>
-                    <div>GPA : 3.93/4.0</div>
+                    <div className="text-muted mb-2">Sep 2017 - Aug 2021</div>
+                    <div><strong>GPA</strong> : 3.93/4.0</div>
+                    <div><strong>Thesis</strong> : Implementation of Naïve Bayes Algorithm on Sentiment Analysis of Topic of 
+                      Giving Covid 19 Pandemic Vaccine Using Python</div>
                   </div>
                 </div>
               </div>
